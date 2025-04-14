a28863429b4c9954526065f9b8f704ba16788d2f
Krupot <pyrrhuselcidgo@gmail.com>
2025-04-14 22:40:40 +0800

    register return to login clear back button position cleared fixing soon during cleanup on the responsiveness on the divs

--- web/scripts/loginInteraction.js
+++ web/scripts/loginInteraction.js
@@ -1,12 +1,19 @@
 document.getElementById("register-in").addEventListener("click", () => {
     x = document.getElementById("login");
     x.style.marginLeft = "-50%";
-    // x.style.visibility = "hidden";
-    // x.style.position = "absolute";
     y = document.getElementById("box");
-    y.style.width = "600px"
+    y.style.width = "400px"
     y.style.height = "700px"
     z = document.getElementById("register");
-    z.style.marginLeft = "12.5%";
+    z.style.marginLeft = "0%";
 
+})
+document.getElementById("back-b").addEventListener("click", () => {
+    x = document.getElementById("login");
+    x.style.marginLeft = "65px";
+    y = document.getElementById("box");
+    y.style.width = "450px"
+    y.style.height = "600px"
+    z = document.getElementById("register");
+    z.style.marginLeft = "50%";
 })
