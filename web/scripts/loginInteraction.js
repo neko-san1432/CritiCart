fe8a1a45baa8ebf25a317d09e20ec3e70ad185fa
Krupot <pyrrhuselcidgo@gmail.com>
2025-04-14 22:11:33 +0800

    idk what I just did

--- web/scripts/loginInteraction.js
+++ web/scripts/loginInteraction.js
@@ -1,12 +1,12 @@
 document.getElementById("register-in").addEventListener("click", () => {
     x = document.getElementById("login");
     x.style.marginLeft = "-50%";
-    x.style.visibility = "hidden";
+    // x.style.visibility = "hidden";
     // x.style.position = "absolute";
     y = document.getElementById("box");
     y.style.width = "600px"
     y.style.height = "700px"
     z = document.getElementById("register");
-    z.style.marginLeft = "0%";
+    z.style.marginLeft = "12.5%";
 
 })
