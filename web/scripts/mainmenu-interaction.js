dbae9cc166751c835fbf632d312d9e7de3289d4a
Krupot <pyrrhuselcidgo@gmail.com>
2025-04-15 21:59:51 +0800

    done login page plus simple placement in the mainmenu

---
+++ web/scripts/mainmenu-interaction.js
@@ -0,0 +1,26 @@
+const slider1 = document.querySelector('.product-holder-list');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();  // Prevent text highlight
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1.5; // adjust scroll speed
+  slider.scrollLeft = scrollLeft - walk;
+});
