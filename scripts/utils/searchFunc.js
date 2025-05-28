c13a9d88d56ed539f036c74b8b673f6054c6d6ec
Krupot <pyrrhuselcidgo@gmail.com>
2025-05-28 21:37:52 +0800

    stop for a day for thesis idea defense

--- scripts/utils/searchFunc.js
+++ scripts/utils/searchFunc.js
@@ -1,6 +1,7 @@
 //task: get the queued search function in the ChatGpt
 import {supabase} from '../api/database.js'
 const searchBar = document.getElementById('')
+const {data:tags,error: retrieveTagsError} = await supabase.from("tags").select("tags").eq();
 searchBar.addEventListener('input',()=>{
     searchBar.value
 })
